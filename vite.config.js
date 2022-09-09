65b5e14c1ceb2692c632fe300e46180c3838a96e
Thanathip Ngamloed <thanathip804@gmail.com>
2022-09-09 17:07:42 +0700

    update 09092022-1

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite"
 import { svelte } from "@sveltejs/vite-plugin-svelte"
-import { fullVersionLink, ogImageUrl } from "./src/data"
+import { ogImageUrl } from "./src/data"
 
 const headers = [
   `<title>Manassarn Manoonchai - Resumette</title>`,
   `<meta name="title" content="Manassarn Manoonchai - Resumette" />`,
   `<meta name="description" content="Interactive & printable résumé with tailored skills fitted in single letter page." />`,
   `<meta name="viewport" content="width=device-width, initial-scale=1" />`,
-  `<meta property="og:url" content=${fullVersionLink} />`,
+  // `<meta property="og:url" content=${fullVersionLink} />`,
   `<meta property="og:type" content="website" />`,
   `<meta property="og:title" content="Manassarn Manoonchai - Resumette" />`,
   `<meta property="og:description" content="Interactive & printable résumé with tailored skills fitted in single letter page." />`,
